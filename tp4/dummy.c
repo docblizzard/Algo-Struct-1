60fb628c205889a34694ed56b38cf74078e00976
docblizzard <44072236+docblizzard@users.noreply.github.com>
2021-10-22 17:36:01 +0200

    Add files via upload

---
+++ tp4/dummy.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void insertion(Liste *liste, int nvNombre);
+
+int main(int argc, char** argv){
+
+}
+typedef struct Element Element;
+struct Element{
+    int nombre;
+    Element *suivant;
+};
+
+typedef struct Liste Liste;
+struct Liste{
+    Element *premier;
+};
+Liste *initialisation()
+{
+    Liste *liste = malloc(sizeof(*liste));
+    Element *element = malloc(sizeof(*element));
+
+    if (liste == NULL || element == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    element->nombre = 0;
+    element->suivant = NULL;
+    liste->premier = element;
+
+    return liste;
+}
+
+void insertion(Liste *liste, int nvNombre);
+    /* Création du nouvel élément */
+    Element *nouveau = malloc(sizeof(*nouveau));
+    if (liste == NULL || nouveau == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    nouveau->nombre = nvNombre;
+
+    /* Insertion de l'élément au début de la liste */
+    nouveau->suivant = liste->premier;
+    liste->premier = nouveau;
+}
