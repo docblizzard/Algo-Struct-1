53d358c8bdd750b94f35ba0f0611a30c24f6093c
docblizzard <44072236+docblizzard@users.noreply.github.com>
2021-12-03 17:16:25 +0100

    Update ex.c

--- TP7/ex.c
+++ TP7/ex.c
@@ -1,98 +1,80 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-typedef struct noeud {
+typedef struct nAVL{
     int val;
-    struct noeud *g;
-    struct noeud *d;
-} Noeud;
-typedef Noeud *Arbre;
+    int h ;
+    struct nAVL *g;
+    struct nAVL *d;
+} NAVL;
 
-void ajouter(Arbre *a, int x);
-int estABR(Arbre a);
-Arbre anul();
-Arbre ass(Arbre gauche, Arbre droit, int etiquette);
-Noeud *Recherche(Arbre a, int x);
-Noeud *extraireMax(Arbre *a);
+typedef NAVL *AVL;
+void MAJHauteur(NAVL *no);
+void ajouter(AVL *ar, int x);
+
 
 
 
 int main(int argc, char** argv){
-    Arbre narbre =NULL;
-    ajouter(&narbre, 42);
-    ajouter(&narbre, 52);
-    ajouter(&narbre,21);
-    ajouter(&narbre, 95);
-    printf("%d\n", narbre->d->d->val);
-    printf("Le pointeur de l'arbre est %p", Recherche(narbre,95));
-    extraireMax(&narbre);
+    AVL a = NULL;
+    ajouter(&a,25);
+    ajouter(&a,20);
+    ajouter(&a,19);
+    ajouter(&a,18);
+    ajouter(&a,17);
 
+    MAJHauteur(a);
+    printf("%d", a->g->h);
 }
 
 
+// EX1
 
+void MAJHauteur(NAVL *no){
+    int hauteurg = 0; int hauteurd = 0;
 
-// EX 1
-void ajouter(Arbre *a, int x){
-    if(*a==NULL)
-    {
-        *a=(Noeud*)malloc(sizeof(Noeud));
-        (*a)->val=x;
-        (*a)->g=NULL;
-        (*a)->d=NULL;
-    }
-    if ((*a)->val ==x)
+    if (!no) {
         return;
-    if((*a)->val<x)
-        ajouter(&(*a)->d,x);
+    }
+    if (!(no->g && no->d)){
+        no->h = 0;
+    }
+    if (no->g){
+        hauteurg = no->g->h;
+    }
+    if (no->d){
+        hauteurd = no->d->h;
+    }
+    if (hauteurd < hauteurg){
+        no->h = no->h + (hauteurg + 1);
+        MAJHauteur(no->g);
+    }
+    else{
+        no->h = no->h + (hauteurd + 1);
+        MAJHauteur(no->d);
+    }
+}
+
+void rotationG(AVL *ar){
+    NAVL * temp = *ar;
+    if ((*ar)->g){
+        (*ar)->g = temp;
+        *ar = temp->g; 
+    }
+}
+
+void ajouter(AVL *ar, int x){
+    if(*ar==NULL){
+        *ar=(NAVL*)malloc(sizeof(NAVL));
+        (*ar)->val=x;
+        (*ar)->g=NULL;
+        (*ar)->d=NULL;
+    }
+    if ((*ar)->val == x)
+        return;
+    if((*ar)->val < x)
+        ajouter(&(*ar)->d,x);
     else
-        ajouter(&(*a)->g,x);
+        ajouter(&(*ar)->g,x);
     
-}
-
-// EX2
-
-/*int estABR(Arbre a){
-if (a == NULL)
-    return 1;
-    int d;
-    return 
-    Arbre *temp = malloc(sizeof(*Arbre));
-    if (temp->val != NULL){
-        if (temp->val > temp->g->val && temp->val < temp->d->val){
-            estABR(temp->g);
-            stABR(temp->d);
-        }
-        else return 0;
-    }
-} */
-
-// EX3
-Noeud *Recherche(Arbre a, int x){
-    if (a == NULL) 
-        return NULL;    
-    
-    if (a->val == x)
-        return a;   
-    
-    if (a->val < x){
-        return Recherche(a->d,x);
-    }
-    else return Recherche(a->g,x);
-    
-}
-// EX4
-Noeud *extraireMax(Arbre *a){
-    if (*a == NULL){
-        return NULL;
-    }
-    if ((*a)->val < (*a)->d->val){
-        return extraireMax(&(*a)->d);
-    }
-    if ((*a)->val > (*a)->d->val){
-        return *a;
-    } 
-    else return extraireMax(&(*a)->d);
-
-
 }
