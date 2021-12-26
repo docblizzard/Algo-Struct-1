595c17d43197feaea4b5647586d86e5ce3174535
docblizzard <stephanelwin@hotmail.fr>
2021-12-26 18:39:53 +0100

    update lab

--- ProjetLabyrinthe/lab.c
+++ ProjetLabyrinthe/lab.c
@@ -11,6 +11,7 @@ typedef struct salle{
     struct salle* S;
     struct salle* E;
     struct salle* O;
+    int num;
 } Salle;
 
 typedef struct knight{
@@ -18,23 +19,31 @@ typedef struct knight{
     int key;
 } Knight;
 
-void labcreation(Salle lab, FILE * plan);
+void labcreation(Salle *lab, FILE * plan);
 
 
 int main(void){
-    Salle lab[LAB_SIZE];
+    Salle lab[6];
     FILE *file = fopen("plan.pln", "r");
-    labcreation(*lab, file);
-    
+    //printf("%d ", lab[3].num);
+    labcreation(lab, file);
     fclose(file);
+    printf("%d", lab[4].num);
 
 }
 // Initialization of the labyrinth
-void labcreation(Salle lab, FILE * plan){
+void labcreation(Salle *lab, FILE * plan){
     char string[9];
-    int num, N, S, E, O;
-    if (fscanf(plan,"%s",string)){
-        if (strcmp(string,"debutPlan")==0) printf("True");
-    }
+    int num; char name [2]; char direction1; char direction2;
+    int room1; int room2;
+    if (fscanf(plan,"%s",string)  && (strcmp(string,"debutPlan")==0)) {
+        while ( fscanf(plan, "%s %d.%c-%d.%c;",name, &room1, &direction1, &room2, &direction2) == 5){
+            lab[room1].num = room1;
+            lab[room2].num = room2;
+            lab[room1].N = &lab[room2];
+            lab[room2].S = &lab[room1];
+            }
+        }
+            
     else printf("False");
 }
