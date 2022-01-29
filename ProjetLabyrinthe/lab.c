719e821dc39607e7965bc4f2eae4af009a190485
docblizzard <stephanelwin@hotmail.fr>
2022-01-30 00:03:12 +0100

    fixed issue with segmentation fault

--- ProjetLabyrinthe/lab.c
+++ ProjetLabyrinthe/lab.c
@@ -4,11 +4,11 @@
 #include <string.h>
 #include <ctype.h>
 
-#define LAB_SIZE 10
+#define erreur(a) erreurT(a,__LINE__)
+
 
 // Struct pour les salles
 typedef struct salle{
-    int pos [LAB_SIZE];
     struct salle* N;
     struct salle* S;
     struct salle* E;
@@ -22,8 +22,13 @@ typedef struct knight{
     int key;
 } Knight;
 
-void fillempty(char arr[], char charact);
+int nb_fichiers_ouverts=0;
+
+void erreurT(char*  a,int n);
+FILE* ouvrir(char* nom,char* mode);
+void fermer(FILE* f);
 void viderBuffer();
+void creerGrapheC(Salle *lab,char const * descr);
 void restart(int *loop);
 void narratepath(Salle *lab, Knight player);
 void narratepathupgrade(Salle *lab, Knight player);
@@ -36,10 +41,51 @@ int main(void){
     Salle lab[35] = {0};
     int l = 0;
     int sizelab = sizeof(lab)/sizeof(lab[0]);
-    FILE *file = fopen("plan.pln", "r");
-    labcreation(lab, file);
-    fclose(file);
+    //FILE *file = fopen("plan.pln", "r");
+    creerGrapheC(lab,
+    "debutPlan "
+"A1 0.N-1.S; "
+"A2 1.O-2.E; "
+"A3 2.N-3.S; " 
+"B1 3.E-4.O; "
+"B2 4.N-5.S; "
+"B3 5.O-6.E; "
+"A4 6.N-7.S; " 
+"A4 7.O-8.E; "
+"A4 8.S-9.N; "
+"A4 9.S-10.N; "
+"A4 10.S-11.N; "
+"A4 11.S-12.N; "
+"A4 7.N-13.S; "
+"A4 13.O-14.E; "
+"A4 14.N-15.S; "
+"A4 15.O-16.E; "
+"A4 16.O-17.E; "
+"A4 17.N-18.S; "
+"A4 18.O-19.E; "
+"A4 19.N-20.S; "
+"A4 20.E-21.O; "
+"A4 21.E-22.O; "
+"A4 22.S-23.N; "
+"A4 23.E-24.O; "
+"A4 24.E-25.O; "
+"A4 25.N-26.S; "
+"A4 26.E-27.O; "
+"A4 27.S-28.N; "
+"A4 27.E-29.O; " 
+"A4 27.N-34.S; "
+"A4 29.E-30.O; "
+"A4 30.E-31.O; "
+"A4 30.S-32.N; "
+"A4 30.N-33.S; "
+"A4 34.O-35.E; "
+"finPlan "
+);
+    //printf("%s", lab[0]);
+    //labcreation(lab, file);
+    //fclose(file);
     mainloop(lab, sizelab);
+    assert(nb_fichiers_ouverts==0);
     
 }
 // Lecture du fichier texte et convertir en salles du labyrinthe
@@ -102,13 +148,15 @@ void mainloop(Salle *lab, int sizelab){
         int found = 0;
         printf("Tu te retrouves à l'entrée du chateau labyrinthe.\n\n");
         while(!found){
-            char dir = 0;
+            char dir[1];
             narratepathupgrade(lab, player);
             printf("Dans quelle direction aller? :");
+            //lire(dir, 1);
             scanf("%[n-s-e-o-N-S-E-O]%c",&dir);
-            dir = toupper(dir);
+            dir[0] = toupper(dir[0]);
+            printf("%c", dir[0]);
             // Après avoir tapé un charactère, celui ci est comparé avec un switch, si il est faux ou le chemin n'existe pas il est redemandé
-            switch(dir){
+            switch(dir[0]){
                 case 'N':
                     // Verifie que la salle demandé existe
                     if (lab[player.pos].N){
@@ -224,4 +272,44 @@ void restart(int *loop){
     if (decision == 'Y') *loop = 0;
 
     if (decision == 'N') *loop = 1;
+}
+
+void creerGrapheC(Salle *lab,char const * descr){
+   int i = 0;
+   FILE* f=ouvrir("tp9grapheTest.txt","w");
+
+   while(descr[i]!='\0')
+         putc(descr[i++],f);
+   fermer(f);
+
+   f=ouvrir("tp9grapheTest.txt","r");
+   labcreation(lab,f);
+   fermer(f);
+}
+void fermer(FILE* f){
+   // pour fermer un fichier
+   fclose(f);
+   nb_fichiers_ouverts--;
+   assert(nb_fichiers_ouverts>=0);
+}
+
+FILE* ouvrir(char* nom,char* mode){
+   // pour ouvrir flux (souvent un fichier)
+   // et compter le nombre de flux ouverts
+   FILE* f=fopen(nom,mode);
+   if(f==NULL)
+   {
+     fprintf(stderr,"impossible d'ouvrir le fichier %s en mode %s\n",nom,mode);
+     erreur("erreur d'entree sortie\n");
+   }
+   nb_fichiers_ouverts++;
+   return f;
+}
+
+void erreurT(char* a,int n)
+{
+   fprintf(stderr,"erreur ligne %d\n",n);
+   fprintf(stderr,"%s",a);
+   fprintf(stderr,"terminaison de l'exécution\n");
+   exit(EXIT_FAILURE);
 }
